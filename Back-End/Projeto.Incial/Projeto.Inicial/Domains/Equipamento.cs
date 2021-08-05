0accb60dcb12a3f9713de7f0ef948bad3b7d558c
KemillyFerreira <kemilly.sf@gmail.com>
2021-08-05 12:51:24 -0300

    banco de dados finalizado e WebApi iniciada

---
+++ Back-End/Projeto.Incial/Projeto.Inicial/Domains/Equipamento.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Projeto.Inicial.Domains
+{
+    public partial class Equipamento
+    {
+        public int IdEquipamento { get; set; }
+        public int? IdTipoEquipamento { get; set; }
+        public int? IdSala { get; set; }
+        public string Nome { get; set; }
+        public string Marca { get; set; }
+        public string NumeroSerie { get; set; }
+        public string Descricao { get; set; }
+        public string NumeroPatrimonio { get; set; }
+        public bool? Situacao { get; set; }
+
+        public virtual Sala IdSalaNavigation { get; set; }
+        public virtual TipoEquipamento IdTipoEquipamentoNavigation { get; set; }
+    }
+}
---
+++ Back-End/Projeto.Incial/Projeto.Inicial/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,19 @@
+﻿using Projeto.Inicial.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projeto.inicial.webApi.Interfaces
+{
+    interface IUsuarioRepository
+    {
+        /// <summary>
+        /// validação do usuário
+        /// </summary>
+        /// <param name="email"> email do usuário </param>
+        /// <param name="senha"> senha do usuário </param>
+        /// <returns></returns>
+        Usuario Login(string email, string senha);
+    }
+}

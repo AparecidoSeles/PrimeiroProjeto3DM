4ce6a1b420c55f12e2effc0fd5baacb4ad74f05b
Kemilly Ferreira <kemilly.sf@gmail.com>
2021-08-06 09:40:38 -0300

    interfaces finalizadas

---
+++ Back-End/Projeto.Incial/Projeto.Inicial/Interfaces/ITipoEquipamento.cs
@@ -0,0 +1,37 @@
+﻿using Projeto.Inicial.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto.Incial.Interfaces
+{
+    interface ITipoEquipamento
+    {
+        /// <summary>
+        /// lista todos os tipos de equipamentos existentes
+        /// </summary>
+        /// <returns></returns>
+        List<TipoEquipamento> Listar();
+
+        /// <summary>
+        /// cadastra um novo tipo de equipamento
+        /// </summary>
+        /// <param name="novoTipoEquipamento"> será o novo objeto cadastrado </param>
+        void Cadastrar(TipoEquipamento novoTipoEquipamento);
+
+        /// <summary>
+        /// atualiza um tipo de equipamento existente
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="TipoEquipamentoAtualizado"> será o objeto com a nova informação </param>
+        void Atualizar(int id, TipoEquipamento TipoEquipamentoAtualizado);
+
+        /// <summary>
+        /// deleta um tipo de equipamento existente
+        /// </summary>
+        /// <param name="id"> id do tipo de equipamento que será deletado </param>
+        void Deletar(int id);
+
+    }
+}
